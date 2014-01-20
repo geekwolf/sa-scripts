162237136425e965c6cd9dc564d8d94248595f6c
geekwolf <geekwolf@163.com>
2014-01-20 15:04:14 +0800

    Update mpass.sh

--- linux/mkpasswd/mpass.sh
+++ linux/mkpasswd/mpass.sh
@@ -2,12 +2,8 @@
 #pssh.txt:Host IP
 while read line 
 do
-
-
 a=`mkpasswd -l 16 -c 2 -C 2 -s 1 -v`
 echo  -e  "$line \t  $a">>nowpassw.txt
 pssh -H $line   -P  -p 1 "echo $a|passwd --stdin root"
 echo $line
-
-
 done < pssh.txt

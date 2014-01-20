2ecd60dc2c9cb734cfa4dffd18ef2a840d40a583
geekwolf <geekwolf@163.com>
2014-01-20 14:46:36 +0800

    mkpasswd

---
+++ linux/mkpasswd/mpass.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#pssh.txt:Host IP
+while read line 
+do
+
+
+a=`mkpasswd -l 16 -c 2 -C 2 -s 1 -v`
+echo  -e  "$line \t  $a">>nowpassw.txt
+pssh -H $line   -P  -p 1 "echo $a|passwd --stdin root"
+echo $line
+
+
+done < pssh.txt
